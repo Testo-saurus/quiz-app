d44068d6618447dcebd8b90e6e8120e929b730a2
Jannik Strohbeck <jannikstrohbeck@gmail.com>
2025-06-06 10:15:48 +0200

    make pretty

--- js/addCard.js
+++ js/addCard.js
@@ -1,6 +1,6 @@
-form.addEventListener("submit", (e) => {
-  const form = document.getElementById("form");
+const form = document.getElementById("form");
 
+form.addEventListener("submit", (e) => {
   e.preventDefault();
 
   const question = e.target.question.value;
@@ -10,6 +10,22 @@ form.addEventListener("submit", (e) => {
 
   console.log(question, answer, tag);
 
+  //sucess message
+  const successMessage = document.createElement("div");
+  successMessage.classList.add("success-message");
+  successMessage.textContent = "Card created!";
+  successMessage.style.backgroundColor = "white";
+  successMessage.style.position = "fixed";
+  successMessage.style.top = "5px";
+  successMessage.style.zIndex = "100";
+
+  successMessage.style.border = "solid 1px";
+  successMessage.style.borderRadius = "4px";
+  form.append(successMessage);
+  setTimeout(() => {
+    successMessage.remove();
+  }, 2000);
+
   const newCardEl = document.createElement("article");
 
   newCardEl.classList = "card";
@@ -18,9 +34,31 @@ form.addEventListener("submit", (e) => {
   title.innerText = questionTitle;
   newCardEl.append(title);
 
+  const tagDivEl = document.createElement("div");
+  tagDivEl.classList.add("tags");
+
+  const tagSpanEl = document.createElement("span");
+  tagSpanEl.classList.add("tag");
+  tagSpanEl.innerText = tag;
+
+  tagDivEl.append(tagSpanEl);
+  newCardEl.append(tagDivEl);
+
   const questionEl = document.createElement("p");
   questionEl.innerText = question;
   newCardEl.append(questionEl);
 
+  const answerEl = document.createElement("p");
+  answerEl.innerText = answer;
+  answerEl.classList.add("answer", "hidden");
+  newCardEl.append(answerEl);
+
+  const buttonEl = document.createElement("button");
+  buttonEl.classList.add("card-button");
+  buttonEl.innerText = "Show Answer";
+  newCardEl.append(buttonEl);
+
   document.querySelector(".main-content").append(newCardEl);
+
+  form.reset();
 });
